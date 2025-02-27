49a46803d92295b222dabbc7d06ee52d300a2502
Jason Bell <jason.bell@treeloop.com>
2025-02-27 13:59:37 -0500

    Other endpoints

--- EComm.API/Controllers/ProductController.cs
+++ EComm.API/Controllers/ProductController.cs
@@ -2,7 +2,7 @@
 
 namespace EComm.API.Controllers;
 
-//[ApiController]
+[ApiController]
 public class ProductController(IECommDb db) : ControllerBase
 {
     [HttpGet("/products")]
@@ -18,5 +18,53 @@ public class ProductController(IECommDb db) : ControllerBase
     {
         var product = await db.GetProduct(id);
         return product is null ? NotFound() : Ok(product);
+    }
+
+    [HttpPost("/products")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> CreateProduct(Product product)
+    {
+        await db.AddProduct(product);
+        return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+    }
+
+    [HttpPut("/products/{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<ActionResult> PutProduct(int id, Product product)
+    {
+        var existingProduct = await db.GetProduct(id);
+        if (existingProduct is null)
+        {
+            await db.AddProduct(product);
+            return CreatedAtAction("GetTodo", new { id = product.Id }, product);
+        }
+        else
+        {
+            existingProduct.ProductName = product.ProductName;
+            existingProduct.UnitPrice = product.UnitPrice;
+            existingProduct.Package = product.Package;
+            existingProduct.IsDiscontinued = product.IsDiscontinued;
+            await db.UpdateProduct(existingProduct);
+            return NoContent();
+        }
+    }
+
+    [HttpDelete("/products/{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var existingProduct = await db.GetProduct(id);
+        if (existingProduct is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            await db.DeleteProduct(existingProduct);
+            return NoContent();
+        }
     }
 }
