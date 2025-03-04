05f0e1d8fc1b8f7e4afad3f5d7e3ec3314538548
Jason Bell <jason.bell@treeloop.com>
2025-03-04 15:45:31 -0500

    Edit product

--- EComm.MvcUI/Controllers/ProductController.cs
+++ EComm.MvcUI/Controllers/ProductController.cs
@@ -2,20 +2,54 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
-namespace EComm.MvcUI.Controllers
-{
-    public class ProductController(IECommDb db) : Controller
-    {
-        [HttpGet("product/{id}")]
-        public async Task<IActionResult> Details(int id)
-        {
-            var product = await db.GetProduct(id);
-            if (product is null)
-            {
-                return NotFound();
-            }
+namespace EComm.MvcUI.Controllers;
 
+public class ProductController(IECommDb db) : Controller
+{
+    [HttpGet("product/{id}")]
+    public async Task<IActionResult> Details(int id)
+    {
+        var product = await db.GetProduct(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    [HttpGet("product/edit/{id}")]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var product = await db.GetProduct(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    [HttpPost("product/edit/{id}")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Product product)
+    {
+        var existingProduct = await db.GetProduct(id);
+        if (existingProduct is null)
+        {
+            return NotFound();
+        }
+        if (!ModelState.IsValid)
+        {
             return View(product);
         }
+
+        existingProduct.ProductName = product.ProductName;
+        existingProduct.UnitPrice = product.UnitPrice;
+        existingProduct.Package = product.Package;
+        existingProduct.IsDiscontinued = product.IsDiscontinued;
+        await db.UpdateProduct(existingProduct);
+
+        return RedirectToAction("Details", new { id = id});
     }
 }
