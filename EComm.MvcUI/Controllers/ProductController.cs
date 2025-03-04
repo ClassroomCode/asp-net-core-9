ea9dc1789a8802f3ece798ccc61cc80b3ea7537a
Jason Bell <jason.bell@treeloop.com>
2025-03-04 13:46:29 -0500

    Product details

---
+++ EComm.MvcUI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using EComm.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EComm.MvcUI.Controllers
+{
+    public class ProductController(IECommDb db) : Controller
+    {
+        [HttpGet("product/{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await db.GetProduct(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
